bceea063a02e9689cad90083dd37ec5a9b6a4c63
Fernando <asialer05@hotmail.com>
2025-01-02 11:36:14 -0500

    Proyecto base

---
+++ src/main/java/com/fernando/ms/dealers/app/dfood_dealers_service/DfoodDealersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.dealers.app.dfood_dealers_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfoodDealersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfoodDealersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/dealers/app/dfood_dealers_service/DfoodDealersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.dealers.app.dfood_dealers_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfoodDealersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
